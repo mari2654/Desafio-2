e64bb0c534f78a6ef9e424cc2a46372564dac53f
mari2654 <mariana.santos2654@gmailcom>
2022-06-13 09:01:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let idade=18;
+
+if(idade >=18){
+console.log("Pode entrar na festa!");
+}else{
+    console.log("Não pode entrar na festa!");
+}
+
+
+let maiorIdade=true;
+console.log(!maiorIdade);
+
+let mes="Dezembro";
+if((mes===Janeiro)||(mes==="Dezembro")){
+    console.log("Faz aniversário em jan e dez");
+}else{
+    console.log("Não faz aniversário em jan ou dez");
+}
+
+let nome="Raisa";
+if (inicialNome==="R"){
+    console.log("Sua inicial é R");
+}else{
+console.log("Sua inicial não é R");
+}
+
+let (nome="Eduarda Caetano");
+if (letraSobrenome >= 6)||(inicialNome==="E"){
+console.log("Seu sobrenome tem mais de 6 letras e/ou sua inicial é E");
+}else{
+console.log("Seu sobrenome tem menos de 6 letras e/ou sua inicial não é E");   
+}
