d5ecb638a6d7446ad8e1be87cc3ed1310a4c8388
mari2654 <mariana.santos2654@gmailcom>
2022-07-13 13:37:40 -0300

    second commit

--- script.js
+++ script.js
@@ -11,22 +11,22 @@ let maiorIdade=true;
 console.log(!maiorIdade);
 
 let mes="Dezembro";
-if((mes===Janeiro)||(mes==="Dezembro")){
+if((mes==="Janeiro")||(mes==="Dezembro")){
     console.log("Faz aniversário em jan e dez");
 }else{
     console.log("Não faz aniversário em jan ou dez");
 }
 
-let nome="Raisa";
-if (inicialNome==="R"){
-    console.log("Sua inicial é R");
+let name="Raisa";
+if(name.charAt(0) === "R"){
+    console.log(name, "seu nome começa com a letra R");
 }else{
 console.log("Sua inicial não é R");
 }
 
-let (nome="Eduarda Caetano");
-if (letraSobrenome >= 6)||(inicialNome==="E"){
-console.log("Seu sobrenome tem mais de 6 letras e/ou sua inicial é E");
+let sobrenome="Silva";
+if( (sobrenome.length >= 6) || (nome.charAt(0) === "E") ){
+    console.log(`Seu sobrenome: ${sobrenome} tem +6 letras ou começa com a letra E`);
 }else{
-console.log("Seu sobrenome tem menos de 6 letras e/ou sua inicial não é E");   
+    console.log(`Seu sobrenome: ${sobrenome} não tem +6 letras ou não começa com a letra E`)
 }
